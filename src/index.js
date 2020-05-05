d59af31cbd769b725229faf02b9059a71e39b3c1
lazaronazareno <lazaro@Gatitos.localdomain>
2020-05-04 22:50:57 -0300

    borrado directorio innecesario

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Nubi from './App';
-import './index.css';
-
-ReactDOM.render(
-  <Nubi />,
-  document.getElementById('root')
-);
