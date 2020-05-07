0ac2fa17135c40de44b32d71c3a8e6fcbfe23cbe
rilmarramos <rilmarramoslastra@gmail.com>
2020-05-07 01:28:16 -0300

    Arregle el tamaño de fondo del fondo

--- nubi/src/Components/Blog/ContainerOne/index.js
+++ nubi/src/Components/Blog/ContainerOne/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Fondo from '../assets/Fondo.png'
 
 class ContainerOne extends React.Component{
     render(){
         return (
             <div className={styles.cont1}>
                 <div className={styles.ContainerBlog}>
-                   <img src={Fondo} className={styles.Fondo}/>
                 </div>
             </div>
         )
