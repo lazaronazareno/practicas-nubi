64c0588c19921e7b9132914695b6bfe81f7de2fb
lazaronazareno <lazaro@Gatitos.localdomain>
2020-05-08 10:59:56 -0300

    corregida indentacion, agregado /constants/colors

--- nubi/src/App.js
+++ nubi/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import CheckIn from './CheckIn'
+
+import CheckIn from './Components/CheckIn/index';
 import styles from './styles.module.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <div className={styles.Appheader}>
-          <CheckIn/>
+          <CheckIn />
         </div>
       </div>
     );
--- nubi/src/Components/CheckIn/ContainerOne/index.js
+++ nubi/src/Components/CheckIn/ContainerOne/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import styles from './styles.module.scss'
 
+import styles from './styles.module.scss';
 
-class ContainerOne extends React.Component{
-    render(){
-        return (
-            <div className={styles.cont1}>
-                    <div className={styles.titleOne}>
-                        <h1 className={styles.title1}>Tu cuenta Nubi en sólo 2 minutos</h1>
-                    </div>
-                    <div className={styles.button}>
-                        <button className={styles.button1}>
-                            Registrate Greatis
-                        </button>
-                    </div>
-            </div>
-        )
-    }
-
+// eslint-disable-next-line react/prefer-stateless-function
+class ContainerOne extends React.Component {
+  render() {
+    return (
+      <div className={styles.cont1}>
+        <div className={styles.titleOne}>
+          <h1 className={styles.title1}>Tu cuenta Nubi en sólo 2 minutos</h1>
+        </div>
+        <div className={styles.button}>
+          <button type="button" className={styles.button1}>
+            Registrate Gratis
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default ContainerOne;
