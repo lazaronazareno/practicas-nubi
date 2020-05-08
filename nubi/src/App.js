5777fb7e2c844a2bbc3d073adc37ac73a04a70c1
lazaronazareno <lazaro@Gatitos.localdomain>
2020-05-08 09:35:12 -0300

    corregidos errores de indentacion, agregado la constante para monopolizar los colores, algunas clases eran las mismas asi que las unifique

--- nubi/src/App.js
+++ nubi/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Benefits from './Benefits'
+
+import Benefits from './components/Benefits';
 import styles from './styles.module.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <div className={styles.Appheader}>
-           <Benefits/>
+          <Benefits />
         </div>
       </div>
     );
--- nubi/src/components/Benefits/ContainerOne/index.js
+++ nubi/src/components/Benefits/ContainerOne/index.js
@@ -1,42 +1,47 @@
 import React from 'react';
+
 import styles from './styles.module.scss';
 
-
-class ContainerOne extends React.Component{
-    render() {
-        return(
-            <div className={styles.cont1}>
-                <div className={styles.containerBenefits}>
-                    <div className={styles.contOne}>
-                     <h1 className={styles.title1}>Ràpido.</h1>
-                     <hr className={styles.hr1}></hr>
-                     <p>Realizà tu recagara o retiro de tu cuenta PayPal con solo unos clicks.</p>
-                    </div>
-                
-                   <div className={styles.contTwo}>
-                       <h1 className={styles.title2}>Simple.</h1>
-                       <hr className={styles.hr2}></hr>
-                       <p>Olvidate de los papeleos inagotables.Con Nubi hacè todo en 4 simples pasos desde donde quieras.</p>
-                   </div>
-                   <div className={styles.contThree}>
-                       <h1 className={styles.title3}>Seguro.</h1>
-                       <hr className={styles.hr3}></hr>
-                       <p>Contamos con monitoreo de fraudes 24/7 y cubrimos 100% de las transacciones no autorizadas verificadas.</p>
-                   </div>
-                   <div className={styles.contFour}>
-                       <h1 className={styles.title4}>Transparente.</h1>
-                       <hr className={styles.hr4}></hr>
-                       <p>Conoce dònde esta tu dinero en todo momento. Sin comisiones ocultas.</p>
-                   </div>
-                   <div className={styles.botton}>
-                       <button className={styles.botton1}>Registrate Gratis</button>
-                   </div>
-                
-                
-                </div>            
-            </div>
-        )
-    }
+// eslint-disable-next-line react/prefer-stateless-function
+class ContainerOne extends React.Component {
+  render() {
+    return (
+      <div className={styles.cont1}>
+        <div className={styles.containerBenefits}>
+          <div className={styles.contOne}>
+            <h1 className={styles.title}>Ràpido.</h1>
+            <div className={styles.hr} />
+            <p>Realizà tu recagara o retiro de tu cuenta PayPal con solo unos clicks.</p>
+          </div>
+          <div className={styles.contTwo}>
+            <h1 className={styles.title}>Simple.</h1>
+            <div className={styles.hr} />
+            <p>
+              Olvidate de los papeleos inagotables.Con Nubi hacè todo en 4 simples pasos desde donde quieras.
+            </p>
+          </div>
+          <div className={styles.contThree}>
+            <h1 className={styles.title}>Seguro.</h1>
+            <div className={styles.hr} />
+            <p>
+              Contamos con monitoreo de fraudes 24/7 y cubrimos 100% de las transacciones no autorizadas
+              verificadas.
+            </p>
+          </div>
+          <div className={styles.contFour}>
+            <h1 className={styles.title}>Transparente.</h1>
+            <div className={styles.hr} />
+            <p>Conoce dònde esta tu dinero en todo momento. Sin comisiones ocultas.</p>
+          </div>
+          <div className={styles.botton}>
+            <button type="button" className={styles.botton1}>
+              Registrate Gratis
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContainerOne;
