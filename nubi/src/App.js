d6eb9d5a5312060c369cdc490872c8627e9df9ce
lazaronazareno <lazaro@Gatitos.localdomain>
2020-05-10 13:55:10 -0300

    agregado linter, corregida indentacion, borrado archivo innecesario

---
+++ nubi/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Footer from './Components/Footer';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
--- nubi/src/Components/Footer/index.js
+++ nubi/src/Components/Footer/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import styles from './styles.module.scss'
-import ContainerOne from './ContainerOne'
 
-function Footer(){
-    return(
-        <div className={styles.Footer}>
-            <ContainerOne/>
-        </div>
-    )
+import styles from './styles.module.scss';
+import ContainerOne from './ContainerOne';
+
+function Footer() {
+  return (
+    <div className={styles.Footer}>
+      <ContainerOne />
+    </div>
+  );
 }
 
 export default Footer;
